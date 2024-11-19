a43a9d73d7a308290fbc8649d2b15479c36c4c7d
Tapajoyti Chowdhury <tchowdhury@tnsiit.com>
2024-11-19 12:02:31 +1100

    change main

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi("TC")
+    print_hi("TC1")
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
