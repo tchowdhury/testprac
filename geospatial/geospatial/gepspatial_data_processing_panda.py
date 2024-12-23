c485d3b031c17ea5292702732fff8a4b5ee0012b
Tapajoyti Chowdhury <tchowdhury@tnsiit.com>
2024-12-23 18:42:20 +1100

    added more modules

--- geospatial/geospatial/gepspatial_data_processing_panda.py
+++ geospatial/geospatial/gepspatial_data_processing_panda.py
@@ -7,11 +7,17 @@ import contextily
 restaurants = pd.read_csv('../data/Paris/paris_restaurants.csv')
 
 # Inspect the first rows of restaurants
-print(restaurants.head(1))
+#print(restaurants.head())
+
+#print(restaurants['type'].value_counts())
+
+#filt_restaurants = restaurants[(restaurants['type']=='Caribbean restaurant') & (restaurants['type']=='European restuarant')]
+#print(filt_restaurants.head())
+
 
 # Make a plot of all points
-fig, ax = plt.subplots(figsize=(18,12))
-ax.plot(restaurants['x'],restaurants['y'], 'o', markersize=1)
-# Add all restaurants with map at the background
-contextily.add_basemap(ax)
-plt.show()
+# fig, ax = plt.subplots(figsize=(18,12))
+# ax.plot(restaurants['x'],restaurants['y'], 'o', markersize=1)
+# # Add all restaurants with map at the background
+# contextily.add_basemap(ax)
+# plt.show()
--- geospatial/geospatial/handling_vector_geospatial_data_geopanda.py
+++ geospatial/geospatial/handling_vector_geospatial_data_geopanda.py
@@ -1,17 +1,33 @@
 # Import all packages
-import pandas as pd
+import geopandas
 import matplotlib.pyplot as plt
 import contextily
+import pandas as pd
+
+# Read the geojson file
+districts = geopandas.read_file('../data/Paris/paris_districts_utm.geojson')
 
 # Read the restaurants csv file
-restaurants = pd.read_csv('../data/Paris/paris_restaurants.csv')
+df = pd.read_csv('../data/Paris/paris_restaurants.csv')
+
+# Convert it to a GeoDataFrame
+restaurants = geopandas.GeoDataFrame(df, geometry=geopandas.points_from_xy(df.x, df.y))
 
 # Inspect the first rows of restaurants
-print(restaurants.head(1))
+print(districts.head())
 
-# Make a plot of all points
-fig, ax = plt.subplots(figsize=(18,12))
-ax.plot(restaurants['x'],restaurants['y'], 'o', markersize=1)
-# Add all restaurants with map at the background
+# Inspect the first rows of the restaurants GeoDataFrame
+print(restaurants.head())
+
+# get area undr each plygon
+#print(districts.geometry.area)
+
+# Make a plot of all districts
+districts.plot("district_name")
+
+
+# Make a plot of the restaurants
+ax = restaurants.plot(markersize=1)
 contextily.add_basemap(ax)
 plt.show()
+
