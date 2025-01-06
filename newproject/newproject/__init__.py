b179617eb5072fe2edecfd241d0960d0a9d54d88
Tapajoyti Chowdhury <tchowdhury@tnsiit.com>
2025-01-06 18:14:41 +1100

    added more modules

--- newproject/newproject/__init__.py
+++
@@ -1,3 +0,0 @@
-__version__ = '0.0.1'
-
-from .helloworld import print_hi, listThing
--- newproject/tests/test_createthings_version.py
+++
@@ -1,4 +0,0 @@
-from newproject.newproject import __version__
-
-def test_version():
-    assert __version__ == '0.0.1'
